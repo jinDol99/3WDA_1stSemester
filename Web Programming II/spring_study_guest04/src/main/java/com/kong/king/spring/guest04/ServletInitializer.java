69cb50be32f034b525f7a279e668a32eb8bdb392
jindol <parkmoc21@gmail.com>
2023-04-20 20:50:40 +0900

    ㅇㅇㅇ

---
+++ Web Programming II/spring_study_guest04/src/main/java/com/kong/king/spring/guest04/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.kong.king.spring.guest04;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SpringStudyGuest04Application.class);
+	}
+
+}
